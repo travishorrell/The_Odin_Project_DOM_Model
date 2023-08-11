643b01b7a80778e3fed1cff5fc6616b28c200170
Emergency Software Systems <travishorrell@mac.com>
2023-08-11 11:22:12 -0600

    Updated the DOM using JavaScript

---
+++ JavaScript/IndexJavascript.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('#container');
+const addedRedPar = document.createElement('p');
+const addH3Tag = document.createElement('h3');
+const addPinkDiv = document.createElement('div');
+const h1 = document.createElement('h1');
+const paragraphTag = document.createElement('p');
+
+
+
+//Changing and adding tags to the DOM
+addedRedPar.style.color = 'red';
+addedRedPar.textContent = "Hi i'm Red";
+
+addH3Tag.style.color = "blue";
+addH3Tag.textContent = "I'm blue H3";
+
+addPinkDiv.style.backgroundColor = 'pink';
+addPinkDiv.style.border = "2px solid black";
+
+h1.textContent = "I'm in a div";
+paragraphTag.textContent = "ME TOO";
+
+
+
+//Inserting the Tags into the HTML page
+container.appendChild(addedRedPar);
+container.appendChild(addH3Tag);
+container.append(addPinkDiv);
+addPinkDiv.appendChild(h1);
+addPinkDiv.appendChild(paragraphTag);
+
